d6b0a2adfd44e392d62529bd53e6e5929df9e321
Christoph Bühler <christoph.buehler@unisg.ch>
2025-10-22 15:59:45 +0200

    upgrade

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
   },
 };
 
